edf3772a75bf1c7333e7fb935373d7283ec4fea3
Damiam Alfaro <damiamalfaro@gmail.com>
2025-07-04 20:37:48 -0700

    testing3

--- checking_users.js
+++ checking_users.js
@@ -2,6 +2,6 @@ document.getElementById('user_form').addEventListener("submit", function(e) {
     e.preventDefault();
 
     const formData = FormData(form)
-    alert(formData);
-    
+    console.log(Object.fromEntries(formData));
+
 });
