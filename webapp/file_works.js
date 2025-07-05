af3a71e5a89d404834872d982708b3db1c555998
Damiam Alfaro <damiamalfaro@gmail.com>
2025-07-04 19:55:40 -0700

    now let's see this

--- webapp/file_works.js
+++ webapp/file_works.js
@@ -73,7 +73,7 @@ function storeElapsedTime(time, activity) {
         timestamp: new Date().toISOString(),
     };
 
-    fetch('../api/store-time', {
+    fetch('../webapp/api/store-time', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload),
@@ -104,7 +104,7 @@ const displayActivitiesButton = document.getElementById(
 
 
 function showActivities() {
-  fetch('../api/show-activities')
+  fetch('../webapp/api/show-activities')
     .then((res) => {
       if (!res.ok) throw new Error('Network error');
       return res.json();
@@ -147,7 +147,7 @@ const statisticsButton = document.getElementById(
     "show_statistics_button").addEventListener("click",hoursStatistics);
 
 function hoursStatistics() {
-  fetch('../api/show-activities')
+  fetch('../webapp/api/show-activities')
     .then((res) => {
       if (!res.ok) throw new Error('Network error');
       return res.json();
