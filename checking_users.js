6393c447851f7c5447320269d3c30c952fea6a8f
DamiamAlfaro <damiamalfaro@gmail.com>
2025-07-08 21:23:33 -0700

    i want to see if an existent username exists it will display a message

--- checking_users.js
+++ checking_users.js
@@ -50,6 +50,52 @@ theForm.addEventListener('submit', function(event) {
 
 
 // USER SIGN UP
-alert('this is working');
+const signUpForm = document.getElementById('signUpForm');
+signUpForm.addEventListener('submit', function(event) {
+    event.preventDefault();
 
+    // Define the desired variables that we want to store
+    // within the existing database.
+    const formData = new FormData(this);
+    const newUsername = formData.get('new_username');
+    const newPassword = formData.get('new_password');
+    const newEmail = formData.get('new_email');
+
+    // Now that we have the variables, we need to store them.
+    // However, let's set a rule for each of the variables:
+    // Username: cannot be added if existent.
+    // Password: more than 5 characters.
+    // Email: cannot be added if existent.
+    // Let's start with the username. Let's retrieve the usernames
+    // from the SQL table and check if the inputted one equals
+    // one of them.
+    fetch('/api/user-retrieval')
+    .then((res) => {
+        if (!res.ok) throw new Error('Did not work mate');
+        return res.json();
+    })
+    .then((data) => {
+        data.forEach((row) => {
+            const userNameRetrieved = row.user_name;
+            const userPasswordRetrieved = row.user_password;
+
+            // Now that you have the credentials, as well as the
+            // content from the table, check if they match.
+            if (userNameRetrieved === newUsername) {
+                let existentUsernameMessage = document.createElement("p");
+                existentUsernameMessage.textContent = "Try another username"
+            }
+
+        });
+
+    })
+    .catch((err) => {
+        console.error('Error in the user retrieval in sign up fuck:', err);
+        alert('something came up with user retrieval in sign up FUCK!');
+    })
+
+
+
+
+})
 
