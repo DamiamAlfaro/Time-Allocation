aeec8ee3dd81c9b9b7417eb11f6f7c704cdd486c
DamiamAlfaro <damiamalfaro@gmail.com>
2025-07-09 20:15:52 -0700

    let's try

--- api/store-new-user.js
+++ api/store-new-user.js
@@ -2,8 +2,6 @@ import mysql from 'mysql2/promise';
 
 export default async function handler(req, res) {
 
-    req.method = "POST";
-
     if (req.method !== 'POST') {
         return res.status(405).json({ error: `${req.method}` });
     }
