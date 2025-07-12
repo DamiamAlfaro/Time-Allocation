7957077142445782d01b23c4d34bdcc5ad6c7808
DamiamAlfaro <damiamalfaro@gmail.com>
2025-07-11 20:49:22 -0700

    was not this the same thing?

--- checking_users.js
+++ checking_users.js
@@ -61,8 +61,6 @@ signUpForm.addEventListener('submit', function(event) {
     const newPassword = formData.get('new_password');
     const newEmail = formData.get('new_email');
 
-    let goodToStore = false;
-
     // Now that we have the variables, we need to store them.
     // However, let's set a rule for each of the variables:
     // Username: cannot be added if existent.
@@ -88,21 +86,23 @@ signUpForm.addEventListener('submit', function(event) {
 
         });
 
+        if (existentUsernames.includes(newUsername)) {
+            document.getElementById('messageDisplay').innerHTML = 'Username already exists, try another one...';
+        } else if (existentEmails.includes(newEmail)) {
+            document.getElementById('messageDisplay').innerHTML = 'Email already exists, try a different one...';
+        } else if (newPassword.length <= 5) {
+            document.getElementById('messageDisplay').innerHTML = 'Password must be more than 5 characters mate...';
+        } else {
+            document.getElementById('messageDisplay').innerHTML = 'Finally mate!';
+        };
+
     })
     .catch((err) => {
         console.error('Error in the user retrieval in sign up fuck:', err);
         alert('something came up with user retrieval in sign up FUCK!');
     });
 
-    if (existentUsernames.includes(newUsername)) {
-        document.getElementById('messageDisplay').innerHTML = 'Username already exists, try another one...';
-    } else if (existentEmails.includes(newEmail)) {
-        document.getElementById('messageDisplay').innerHTML = 'Email already exists, try a different one...';
-    } else if (newPassword.length <= 5) {
-        document.getElementById('messageDisplay').innerHTML = 'Password must be more than 5 characters mate...';
-    } else {
-        document.getElementById('messageDisplay').innerHTML = 'Finally mate!';
-    };
+
 
 
 
