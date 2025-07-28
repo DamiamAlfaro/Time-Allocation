ab11c96d8f76a68e1cf36f97b26357564a86593d
DamiamAlfaro <damiamalfaro@gmail.com>
2025-07-27 19:35:35 -0700

    adding timestamp display

--- webapp/file_works.js
+++ webapp/file_works.js
@@ -166,6 +166,7 @@ function showActivities() {
           div.innerHTML = `
             <p>Activity: ${row.activity}</p>
             <p>Time Spent ${row.elapsed_time}</p>
+            <p>Time Stamp ${row.timestamp}</p>
           `;
           container.appendChild(div);
       });
